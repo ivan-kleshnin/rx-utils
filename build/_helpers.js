e3f8a3e885f2fee681efff91395b94244af18d1e
Ivan Kleshnin <ivan@paqmind.com>
2017-10-31 12:09:20 +0200

    Fix build step

--- build/_helpers.js
+++ build/_helpers.js
@@ -1,17 +1,35 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.curryN = curryN;
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 // Curry
-export function curryN(N, fn) {
-  let self = undefined;
-  let collectFn = Object.defineProperties(function (...args) {
+function curryN(N, fn) {
+  var self = undefined;
+  var collectFn = Object.defineProperties(function () {
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
     if (this) {
       self = this;
     }
     if (args.length >= N) {
       return fn.apply(self, args);
     } else {
-      return Object.defineProperties(function (...args2) {
+      return Object.defineProperties(function () {
         if (this) {
           self = this;
         }
+
+        for (var _len2 = arguments.length, args2 = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+          args2[_key2] = arguments[_key2];
+        }
+
         return collectFn.apply(self, args.concat(args2));
       }, {
         name: { value: fn.name + "_" + args.length },
@@ -24,47 +42,71 @@ export function curryN(N, fn) {
   });
   return collectFn;
 }
-export let curry = function (fn) {
+var curry = exports.curry = function curry(fn) {
   return curryN(fn.length, fn);
 };
 
 // Core Fn utils
-export let id = x => x;
-export let always = curry((x, y) => x);
-export let complement = fn => (...args) => !fn(...args);
+var id = exports.id = function id(x) {
+  return x;
+};
+var always = exports.always = curry(function (x, y) {
+  return x;
+});
+var complement = exports.complement = function complement(fn) {
+  return function () {
+    return !fn.apply(undefined, arguments);
+  };
+};
 
 // Low-level helpers
-export let fst = xs => xs[0];
-export let keys = Object.keys;
-export let merge = curry((xs, ys) => Object.assign({}, xs, ys));
-export let range = curry((from, to) => {
-  let rs = [];
-  for (let i = from; i < to; i++) {
+var fst = exports.fst = function fst(xs) {
+  return xs[0];
+};
+var keys = exports.keys = Object.keys;
+var merge = exports.merge = curry(function (xs, ys) {
+  return Object.assign({}, xs, ys);
+});
+var range = exports.range = curry(function (from, to) {
+  var rs = [];
+  for (var i = from; i < to; i++) {
     rs.push(i);
   }
   return rs;
 });
-export let snd = xs => xs[1];
-export let values = Object.values;
+var snd = exports.snd = function snd(xs) {
+  return xs[1];
+};
+var values = exports.values = Object.values;
 
 // High-level helpers
-export let filter = curry((fn, xs) => xs.filter(fn));
-export let isPlainObj = o => Boolean(o && o.constructor && o.constructor.prototype && o.constructor.prototype.hasOwnProperty("isPrototypeOf"));
-export let flattenObj = (obj, keys = []) => {
-  return Object.keys(obj).reduce((acc, key) => {
-    return merge(acc, isPlainObj(obj[key]) ? flattenObj(obj[key], keys.concat(key)) : { [keys.concat(key).join(".")]: obj[key] });
+var filter = exports.filter = curry(function (fn, xs) {
+  return xs.filter(fn);
+});
+var isPlainObj = exports.isPlainObj = function isPlainObj(o) {
+  return Boolean(o && o.constructor && o.constructor.prototype && o.constructor.prototype.hasOwnProperty("isPrototypeOf"));
+};
+var flattenObj = exports.flattenObj = function flattenObj(obj) {
+  var keys = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+
+  return Object.keys(obj).reduce(function (acc, key) {
+    return merge(acc, isPlainObj(obj[key]) ? flattenObj(obj[key], keys.concat(key)) : _defineProperty({}, keys.concat(key).join("."), obj[key]));
   }, {});
 };
-export let map = curry((fn, xs) => xs.map(fn));
-export let mapObjIndexed = curry((fn, obj) => {
-  return reduce((z, k) => {
+var map = exports.map = curry(function (fn, xs) {
+  return xs.map(fn);
+});
+var mapObjIndexed = exports.mapObjIndexed = curry(function (fn, obj) {
+  return reduce(function (z, k) {
     z[k] = fn(obj[k], k, obj);
     return z;
   }, {}, keys(obj));
 });
-export let reduce = curry((fn, z, xs) => xs.reduce(fn, z));
-export let zipObj = curry((keys, values) => {
-  return reduce((z, i) => {
+var reduce = exports.reduce = curry(function (fn, z, xs) {
+  return xs.reduce(fn, z);
+});
+var zipObj = exports.zipObj = curry(function (keys, values) {
+  return reduce(function (z, i) {
     z[keys[i]] = values[i];
     return z;
   }, {}, range(0, keys.length));
--- build/index.js
+++ build/index.js
@@ -1,74 +1,106 @@
-import { Observable as O, Subject as S } from "rxjs";
-import * as R from "./_helpers";
+"use strict";
 
-let sndComplement = R.complement(R.snd);
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.chan = exports.mergeObjTracking = exports.combineLatestObj = exports.mergeObj = exports.passIfDown = exports.passIfUp = exports.passIfLow = exports.passIfHigh = undefined;
+
+var _rxjs = require("rxjs");
+
+var _helpers = require("./_helpers");
+
+var R = _interopRequireWildcard(_helpers);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+var sndComplement = R.complement(R.snd);
 
 // Passes values from `a$` further when `b$` is truthy.
 // passIfHigh :: Obs b -> Obs a -> Obs a
-export let passIfHigh = obs => self => {
-  return self.withLatestFrom(obs).filter(R.snd).map(R.fst);
+var passIfHigh = exports.passIfHigh = function passIfHigh(obs) {
+  return function (self) {
+    return self.withLatestFrom(obs).filter(R.snd).map(R.fst);
+  };
 };
 
 // Passes values from `a$` instance further when `b$` is falsy.
 // passIfLow :: Obs b -> Obs a -> Obs a
-export let passIfLow = obs => self => {
-  return self.withLatestFrom(obs).filter(sndComplement).map(R.fst);
+var passIfLow = exports.passIfLow = function passIfLow(obs) {
+  return function (self) {
+    return self.withLatestFrom(obs).filter(sndComplement).map(R.fst);
+  };
 };
 
 // Passes values from `a$` further when `b$` is truthy, including the switch moment.
 // passIfUp :: Obs b -> Obs a -> Obs a
-export let passIfUp = obs => self => {
-  return self.combineLatest(obs).filter(R.snd).map(R.fst);
+var passIfUp = exports.passIfUp = function passIfUp(obs) {
+  return function (self) {
+    return self.combineLatest(obs).filter(R.snd).map(R.fst);
+  };
 };
 
 // Passes values from `a$` further when `b$` is falsy, including the switch moment.
 // passIfDown :: Obs b -> Obs a -> Obs a
-export let passIfDown = obs => self => {
-  return self.combineLatest(obs).filter(sndComplement).map(R.fst);
+var passIfDown = exports.passIfDown = function passIfDown(obs) {
+  return function (self) {
+    return self.combineLatest(obs).filter(sndComplement).map(R.fst);
+  };
 };
 
 // Merges an object of streams to a stream of objects.
 // mergeObj :: Object (Obs *) -> Obs *
-export let mergeObj = obj => {
+var mergeObj = exports.mergeObj = function mergeObj(obj) {
   obj = R.flattenObj(obj);
-  let values = R.values(obj); // streams
-  return O.merge(...values);
+  var values = R.values(obj); // streams
+  return _rxjs.Observable.merge.apply(_rxjs.Observable, _toConsumableArray(values));
 };
 
 // Combines an object of streams to a stream of objects.
 // combineLatestObj :: Object (Obs *) -> Obs *
-export let combineLatestObj = obj => {
+var combineLatestObj = exports.combineLatestObj = function combineLatestObj(obj) {
   // a nicer analogy of https://github.com/staltz/combineLatestObj/blob/master/index.js
-  let keys = R.keys(obj); // stream names
-  let values = R.values(obj); // streams
-  return O.combineLatest(values, (...args) => {
+  var keys = R.keys(obj); // stream names
+  var values = R.values(obj); // streams
+  return _rxjs.Observable.combineLatest(values, function () {
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
     return R.zipObj(keys, args);
   });
 };
 
 // Merges an object of streams to a stream of objects, keeping the original key data.
 // mergeObjTracking :: Object (Obs *) -> Obs {key :: String, value :: *}
-export let mergeObjTracking = obj => {
-  obj = R.mapObjIndexed((value, key) => {
-    return value.map(data => ({ key, data }));
+var mergeObjTracking = exports.mergeObjTracking = function mergeObjTracking(obj) {
+  obj = R.mapObjIndexed(function (value, key) {
+    return value.map(function (data) {
+      return { key: key, data: data };
+    });
   }, obj);
-  let values = R.values(obj); // streams
-  return O.merge(...values);
+  var values = R.values(obj); // streams
+  return _rxjs.Observable.merge.apply(_rxjs.Observable, _toConsumableArray(values));
 };
 
 // Makes a callable observable.
 // chan :: (Obs a -> Obs b) -> Obs (State -> State)
 // chan :: a -> Promise State
-export let chan = letFn => {
-  let subj = new S();
-  function bus(...callArgs) {
+var chan = exports.chan = function chan(letFn) {
+  var subj = new _rxjs.Subject();
+  function bus() {
+    for (var _len2 = arguments.length, callArgs = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+      callArgs[_key2] = arguments[_key2];
+    }
+
     if (callArgs.length <= 1) {
       subj.next(callArgs[0]); // no return value
     } else {
       subj.next(callArgs); // no return value
     }
   }
-  let obs = letFn(subj);
+  var obs = letFn(subj);
   Object.setPrototypeOf(bus, obs); // support basic calls
   bus.apply = Function.prototype.apply; // support spreads
   return bus;
